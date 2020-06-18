9f8bcb2278ccac207462a0a3fcc85e4f1bcab765
tsugonko <tsugonko@gmail.com>
2020-06-18 12:31:51 -0400

    Added Travis file and modified docker-compose.yml

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        My Baby!
+        Yo Sugar!
         </p>
         <a
           className="App-link"
